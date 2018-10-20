1ca94df8d2d54ae78f4928cd3fa8c156643ad917
vahyno <vahyno@gmail.com>
2018-10-20 15:41:38 -0700

    style: History styling

--- components/History.js
+++ components/History.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, Platform, TouchableOpacity} from 'react-native';
 import {connect} from 'react-redux';
 import {receiveEntries, addEntry} from '../actions';
 import {timeToString, getDailyReminderValue} from '../utils/helpers';
 import {fetchCalendarResults} from '../utils/api';
 import UdacityFitnessCalendar from 'udacifitness-calendar';
+import {white} from '../utils/colors';
+import DateHeader from './DateHeader';
 
 class History extends React.Component {
     componentDidMount(){
@@ -22,18 +24,28 @@ class History extends React.Component {
 
     renderItem = ({today, ...metrics}, formattedDate, key) => {
         return (
-            <View>
+            <View style={styles.item}>
                 {today
-                    ? <Text>{JSON.stringify(today)}</Text>
-                    : <Text>{JSON.stringify(metrics)}</Text>}
+                    ? <View>
+                        <DateHeader date={formattedDate}/>
+                        <Text style={styles.noDatatext}>
+                            {today}
+                        </Text>
+                      </View>
+                    : <TouchableOpacity onPress={()=> console.log('pressed')}>
+                        <Text>{JSON.stringify(metrics)}</Text>
+                    </TouchableOpacity>}
             </View>
         )
     }
 
     renderEmptyDate = (formattedDate) => {
         return(
-            <View>
-                <Text>No Data for this day.</Text>
+            <View style={styles.item}>
+                <DateHeader date={formattedDate} />
+                <Text style={styles.noDatatext}> 
+                    You didn't log any data on this day.
+                </Text>
             </View>
         )
     }
@@ -49,6 +61,29 @@ class History extends React.Component {
         )
     }
 }
+const styles = StyleSheet.create({
+    item: {
+        backgroundColor: white,
+        borderRadius: Platform.OS === 'ios' ? 16 : 2,
+        padding: 20,
+        marginLeft: 10,
+        marginRight: 10,
+        marginTop: 17,
+        justifyContent: 'center',
+        shadowRadius: 3,
+        shadowOpacity: 0.8,
+        shadowColor: 'rgba(0,0,0,0.24)',
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        }
+    },
+    noDatatext: {
+        fontSize: 20,
+        paddingTop: 20,
+        paddingBottom: 20,
+    }
+});
 
 function mapStateToProps (entries) {
     return {
@@ -57,3 +92,4 @@ function mapStateToProps (entries) {
 }
 
 export default connect(mapStateToProps)(History);
+
