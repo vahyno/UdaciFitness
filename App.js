aa51395b57eeef1d6ffe6d3eba3a0dd568cac8b1
vahyno <vahyno@gmail.com>
2018-10-22 15:06:59 -0700

    feat: added navigation to App.js

--- App.js
+++ App.js
@@ -1,10 +1,83 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Platform } from 'react-native';
 import AddEntry from './components/AddEntry';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import reducer from './reducers';
 import History from './components/History';
+import {createMaterialTopTabNavigator, createBottomTabNavigator} from 'react-navigation';
+import {purple, white} from './utils/colors';
+import {FontAwesome, Ionicons} from '@expo/vector-icons';
+
+const TabsIos = createBottomTabNavigator({
+  History: {
+    screen: History,
+    navigationOptions: {
+      tabBarlabel: 'History',
+      tabBarIcon: ({tintColor}) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
+    }
+  }, 
+  AddEntry: {
+    screen: AddEntry,
+    navigationOptions: {
+      tabBarlabel: 'Add Entry',
+      tabBarIcon: ({tintColor}) => <FontAwesome name='plus-square' size={30} color={tintColor} /> 
+    }
+  }
+}, {
+  navigationOptions: {
+    header: null,
+  },
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? purple : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white : purple,
+      shadowColor: 'rgba(0,0,0,0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+});
+
+const TabsAndroid = createMaterialTopTabNavigator({
+  History: {
+    screen: History,
+    navigationOptions: {
+      tabBarlabel: 'History',
+      tabBarIcon: ({tintColor}) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
+    }
+  }, 
+  AddEntry: {
+    screen: AddEntry,
+    navigationOptions: {
+      tabBarlabel: 'Add Entry',
+      tabBarIcon: ({tintColor}) => <FontAwesome name='plus-square' size={30} color={tintColor} /> 
+    }
+  }
+}, {
+  navigationOptions: {
+    header: null,
+  },
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? purple : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white : purple,
+      shadowColor: 'rgba(0,0,0,0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+})
 
 const store = createStore(reducer);
 
@@ -14,7 +87,7 @@ export default class App extends React.Component {
       <Provider store={store}>
         <View style={{flex: 1}}>
           <View style={{height:20}}/>
-          <History />
+          {Platform.OS === 'ios' ? <TabsIos/> : <TabsAndroid/>}
         </View>
       </Provider>
     );
