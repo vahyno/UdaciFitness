bead34a6909a920610efb9806c26f32474b7834a
vahyno <vahyno@gmail.com>
2018-10-23 11:30:20 -0700

    feat: EntryDetail - added header date

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -1,13 +1,26 @@
-import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
 
 class EntryDetail extends Component {
-  render() {
-    return (
-      <View>
-        <Text>Entry Detail</Text>
-      </View>
-    )
-  }
+    static navigationOptions = ({navigation}) => {
+        const {entryId} = navigation.state.params;
+
+        const year = entryId.slice(0,4);
+        const month = entryId.slice(5,7);
+        const day = entryId.slice(8);
+        
+        return {
+            title: `${month}/${day}/${year}`,
+        }
+    }
+
+    render() {
+        return (
+        <View>
+            <Text>Entry Detail - {JSON.stringify(this.props.navigation.state.params.entryId)}</Text>
+        </View>
+        )
+    }
 }
  export default EntryDetail 
