55861d1b728415b9da415070457619a4caf47629
vahyno <vahyno@gmail.com>
2018-10-24 16:39:49 -0700

    feat: Live

--- components/Live.js
+++ components/Live.js
@@ -1,17 +1,59 @@
 import React, {Component} from 'react';
-import {View, Text, ActivityIndicator, TouchableOpacity, StyleSheet} from 'react-native';
+import {View, Text, ActivityIndicator, TouchableOpacity, StyleSheet, Animated} from 'react-native';
 import {Foundation} from '@expo/vector-icons'; 
 import {purple, white} from '../utils/colors';
+import { Location, Permissions } from 'expo';
+import { calculateDirection } from '../utils/helpers';
 
 class Live extends Component {
     state = {
         coords: null,
-        status: 'granted',
+        status: null,
         direction: '',
     }
 
-    askPermission = () => {
+    componentDidMount () {
+        Permissions.getAsync(Permissions.LOCATION)
+          .then(({ status }) => {
+            if (status === 'granted') {
+              return this.setLocation()
+            }
+            this.setState(() => ({ status }));
+            console.log('status:', status);
+            console.warn(this.state);
+            })
+            .catch((error) => {
+            console.warn('Error getting Location permission: ', error)
+            this.setState(() => ({ status: 'undetermined' }))
+            })
+    }
 
+    askPermission = () => {
+        Permissions.askAsync(Permissions.LOCATION)
+            .then(({ status }) => {
+                if (status === 'granted') {
+                return this.setLocation()
+                }
+                this.setState(() => ({ status }))
+                console.warn('status was not granted')
+            })
+            .catch((error) => console.warn('error asking Location permission: ', error))
+    }
+
+    setLocation = () => {
+        Location.watchPositionAsync({
+            enableHighAccuracy: true,
+            timeInterval: 1,
+            distanceInterval: 1,
+        }), (coords) => {
+            const newDirection = calculateDirection(coords.heading);
+            // const {direction} = this.state;
+            this.setState(()=>({
+                coords,
+                status: 'granted',
+                direction: newDirection,
+            }))
+        }
     }
 
     render(){
@@ -55,17 +97,17 @@ class Live extends Component {
                 <View style={styles.directionContainer}>
                     <Text style={styles.header}>You're heading</Text>
                     <Text style={styles.direction}>
-                        North
+                        {direction}
                     </Text>
                 </View>
                 <View style={styles.metricContainer}>
                     <View style={styles.metric}>
                         <Text style={[styles.header, {color: white}]}>Altitude</Text>
-                        <Text style={[styles.subHeader, {color: white}]}>{200} Feet</Text>
+                        <Text style={[styles.subHeader, {color: white}]}>{coords && Math.round(coords.altitude * 3.2808)} Feet</Text>
                     </View>
                     <View style={styles.metric}>
                         <Text style={[styles.header, {color: white}]}>Speed</Text>
-                        <Text style={[styles.subHeader, {color: white}]}>{300} MPH</Text>
+                        <Text style={[styles.subHeader, {color: white}]}>{coords && (coords.speed * 2.2369).toFixed(1)} MPH</Text>
                     </View>
                 </View>
             </View>
